6b3085f4d8ec034c046ad3fe109ea1fc35cd9891
Brian Nguyen <bnguyen06@gmail.com>
2019-06-04 10:20:40 -0700

    update defaults

--- registration.php
+++ registration.php
@@ -8,11 +8,11 @@
  * http://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to codemaster@resolvecommerce.com so we can send you a copy immediately.
+ * to codemaster@resolvepay.com so we can send you a copy immediately.
  *
  * @category  Resolve
  * @package   Resolve_Resolve
- * @copyright Copyright (c) 2016 Resolve, Inc. (http://www.resolvecommerce.com)
+ * @copyright Copyright (c) 2016 Resolve, Inc. (http://www.resolvepay.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
